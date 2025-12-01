585601d395a598cf37208437797804c185802c97
mmcgough8 <mwm9490@icloud.com>
2025-12-01 17:10:36 -0500

    "created pom and sping initializr"

---
+++ sports-equipment/src/main/java/com/zipcode/sports_equipment/SportsEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.zipcode.sports_equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsEquipmentApplication.class, args);
+	}
+
+}
---
+++ sports-equipment/src/test/java/com/zipcode/sports_equipment/SportsEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcode.sports_equipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsEquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
